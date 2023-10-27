d85059d6f70eccb958ec95a4740551a636ad96b2
ldecup <posedeus@hotmail.fr>
2023-10-27 20:48:24 +0200

    Fixed iteration over arrays

--- utils/periph_utils.lua
+++ utils/periph_utils.lua
@@ -2,14 +2,14 @@ local dict = require("dictionnaries")
 
 local function DetectPeriphByType(deviceType)
     if deviceType == "ENERGY_STORE" then
-        for device in dict.ENERGY_STORE do
+        for _, device in pairs(dict.ENERGY_STORE) do
             local side = DetectDevice(device)
             if side ~= nil then
                 return side, device
             end
         end
     elseif deviceType == "ENERGY_PROD" then
-        for device in dict.ENERGY_PROD do
+        for _, device in pairs(dict.ENERGY_PROD) do
             local side = DetectDevice(device)
             if side ~= nil then
                 return side, device
@@ -18,13 +18,13 @@ local function DetectPeriphByType(deviceType)
     else
         local side = DetectDevice(deviceType)
         if side ~= nil then
-            return side, device
+            return side, deviceType
         end
     end
     return nil, nil
 end
 
-function DetectDevice(deviceId)
+function DetectDevice(deviceType)
     for _, side in pairs(peripheral.getNames()) do
         if peripheral.getType(side) == deviceType then
             return side
