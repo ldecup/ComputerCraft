f4a6bf51c41e0bd78d0821eef4dc8982bd429ba5
ldecup <posedeus@hotmail.fr>
2023-10-28 14:03:01 +0200

    Minor fix on gitmanager send

--- Tools/gitmanager.lua
+++ Tools/gitmanager.lua
@@ -25,10 +25,13 @@ while true do
     if (messageTable[1] == config.masterKeyWord and messageTable[2] == config.keyWord) then
         if messageTable[3] == nil then
             chatbox.sendMessage(config.keyWord.." server version "..fileInfo.version.." is UP")
+            sleep(1)
             chatbox.sendMessage(config.keyWord.." HELP for help")
         elseif messageTable[3] == "HELP" then
             chatbox.sendMessage(config.keyWord.." GET <filename>")
+            sleep(1)
             chatbox.sendMessage(config.keyWord.." SEND <filename> <computerID>")
+            sleep(1)
             chatbox.sendMessage(config.keyWord.." GETID")
         elseif messageTable[3] == "GETID" then
             chatbox.sendMessage(config.keyWord.." server ID: "..os.getComputerID())
@@ -36,8 +39,10 @@ while true do
             local filename = messageTable[4]
             if messageTable[3] == "GET" then
                 shell.run("file get "..filename)
+                chatbox.sendMessage(config.keyWord.." "..filename.." received")
             elseif messageTable[3] == "SEND" then
-                shell.run("file send "..filename.." "..message[5])
+                shell.run("file send "..filename.." "..messageTable[5])
+                chatbox.sendMessage(config.keyWord.." "..filename.." sent to "..messageTable[5])
             end
         end
     end
